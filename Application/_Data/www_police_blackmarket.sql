017ada42b12732d9f7e94974d771f77581e11e10
J. C. Nwobodo <jc.nwobodo@gmail.com>
2015-11-25 15:44:15 +0100

    17. Updates database structure

--- Application/_Data/www_police_blackmarket.sql
+++ Application/_Data/www_police_blackmarket.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 17, 2015 at 02:52 AM
+-- Generation Time: Nov 25, 2015 at 03:42 PM
 -- Server version: 10.0.17-MariaDB
 -- PHP Version: 5.5.30
 
@@ -32,12 +32,20 @@ CREATE TABLE `pbm_reports` (
   `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
   `event_time` datetime NOT NULL,
   `report_time` datetime NOT NULL,
-  `categories` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `location` int(16) NOT NULL COMMENT 'site_locations.id',
-  `related_reports` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `news_sources` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `video_links` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `photos` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+  `location` int(16) NOT NULL COMMENT 'site_locations.id'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pbm_reports_meta`
+--
+
+CREATE TABLE `pbm_reports_meta` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `report_id` bigint(20) UNSIGNED NOT NULL,
+  `meta_type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `meta_value` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -87,9 +95,9 @@ CREATE TABLE `site_comments` (
 CREATE TABLE `site_locations` (
   `id` bigint(16) NOT NULL,
   `parent` bigint(16) NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `location_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `slogan` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `location_type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
   `latitude` decimal(5,3) NOT NULL,
   `longitude` decimal(5,3) NOT NULL,
   `status` int(11) NOT NULL
@@ -117,6 +125,38 @@ CREATE TABLE `site_posts` (
   `status` int(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dumping data for table `site_posts`
+--
+
+INSERT INTO `site_posts` (`id`, `parent`, `post_type`, `guid`, `title`, `content`, `excerpt`, `featured_image`, `category`, `author`, `date_created`, `last_update`, `status`) VALUES
+(1, NULL, 'page', 'how-it-works', 'How It Works', 'some text', 'some exerpt', NULL, NULL, 1, 500000, 510000, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `site_sessions`
+--
+
+CREATE TABLE `site_sessions` (
+  `id` int(16) NOT NULL,
+  `session_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_id` int(16) NOT NULL COMMENT 'users.id',
+  `user_type` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'users_access_levels.user_type',
+  `start_time` int(32) NOT NULL,
+  `user_agent` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ip_address` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `last_activity_time` int(32) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `site_sessions`
+--
+
+INSERT INTO `site_sessions` (`id`, `session_id`, `user_id`, `user_type`, `start_time`, `user_agent`, `ip_address`, `last_activity_time`, `status`) VALUES
+(5, '5655b327c70403.28696123', 1, 'admin', 1448406000, 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0', '::1', 1448409661, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -142,6 +182,7 @@ CREATE TABLE `site_users` (
   `id` int(16) NOT NULL,
   `username` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_type` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
   `first_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `last_name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `nickname` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
@@ -156,23 +197,12 @@ CREATE TABLE `site_users` (
   `biography` text COLLATE utf8mb4_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `site_users_sessions`
+-- Dumping data for table `site_users`
 --
 
-CREATE TABLE `site_users_sessions` (
-  `id` int(16) NOT NULL,
-  `session_id` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `user_id` int(16) NOT NULL COMMENT 'users.id',
-  `user_type` int(2) NOT NULL COMMENT 'users_access_levels.user_type',
-  `start_time` int(32) NOT NULL,
-  `user_agent` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `ip_address` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `last_activity_time` int(32) NOT NULL,
-  `status` int(1) NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `site_users` (`id`, `username`, `password`, `user_type`, `first_name`, `last_name`, `nickname`, `gender`, `date_of_birth`, `date_joined`, `place_of_origin`, `place_of_residence`, `email`, `phone`, `photo`, `biography`) VALUES
+(1, 'admin', 'password', 'admin', 'Chukwuemeka', 'Nwobodo', 'Joe', 'M', 500000000, 800000000, 2, 4, 'jc.nwobodo@gmail.com', '08133621591', NULL, 'some text here');
 
 --
 -- Indexes for dumped tables
@@ -182,6 +212,12 @@ CREATE TABLE `site_users_sessions` (
 -- Indexes for table `pbm_reports`
 --
 ALTER TABLE `pbm_reports`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pbm_reports_meta`
+--
+ALTER TABLE `pbm_reports_meta`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -195,10 +231,7 @@ ALTER TABLE `site_categories`
 -- Indexes for table `site_comments`
 --
 ALTER TABLE `site_comments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `comment_post_ID` (`post_id`),
-  ADD KEY `comment_approved_date_gmt` (`status`),
-  ADD KEY `comment_parent` (`parent`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `site_locations`
@@ -212,6 +245,13 @@ ALTER TABLE `site_locations`
 ALTER TABLE `site_posts`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `pamalink` (`guid`);
+
+--
+-- Indexes for table `site_sessions`
+--
+ALTER TABLE `site_sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `session_id` (`session_id`);
 
 --
 -- Indexes for table `site_uploads`
@@ -228,13 +268,6 @@ ALTER TABLE `site_users`
   ADD UNIQUE KEY `photo_id` (`photo`);
 
 --
--- Indexes for table `site_users_sessions`
---
-ALTER TABLE `site_users_sessions`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `session_id` (`session_id`);
-
---
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -243,6 +276,11 @@ ALTER TABLE `site_users_sessions`
 --
 ALTER TABLE `pbm_reports`
   MODIFY `id` int(16) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `pbm_reports_meta`
+--
+ALTER TABLE `pbm_reports_meta`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `site_categories`
 --
@@ -262,7 +300,12 @@ ALTER TABLE `site_locations`
 -- AUTO_INCREMENT for table `site_posts`
 --
 ALTER TABLE `site_posts`
-  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `site_sessions`
+--
+ALTER TABLE `site_sessions`
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT for table `site_uploads`
 --
@@ -272,12 +315,7 @@ ALTER TABLE `site_uploads`
 -- AUTO_INCREMENT for table `site_users`
 --
 ALTER TABLE `site_users`
-  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `site_users_sessions`
---
-ALTER TABLE `site_users_sessions`
-  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
