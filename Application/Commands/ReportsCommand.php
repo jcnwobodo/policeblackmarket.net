dc2722d9a5b4f86f4d84fd85ffb9709d93ca4965
J. C. Nwobodo <jc.nwobodo@gmail.com>
2015-12-04 18:24:01 +0100

    43. Implement single-report-view and comment submission for reports

--- Application/Commands/ReportsCommand.php
+++ Application/Commands/ReportsCommand.php
@@ -9,19 +9,76 @@
 
 namespace Application\Commands;
 
+use Application\Models\Comment;
 use System\Request\RequestContext;
 use Application\Models\Report;
+use Application\Models\User;
+use System\Utilities\DateTime;
 
 class ReportsCommand extends Command
 {
     protected function doExecute(RequestContext $requestContext)
     {
-        $approved_reports = Report::getMapper('Report')->findByStatus(Report::STATUS_APPROVED);
-
         $data = array();
-        $data['reports'] = $approved_reports;
+        if($requestContext->fieldIsSet('id'))
+        {
+            $report = Report::getMapper('Report')->find($requestContext->getField('id'));
+            if(is_object($report) and $report->getStatus() == Report::STATUS_APPROVED)
+            {
+                if($requestContext->fieldIsSet('post_comment'))
+                {
+                    $author_name = $requestContext->getField('author_name');
+                    $author_email = $requestContext->getField('author_email');
+                    $author_comment = $requestContext->getField('author_comment');
 
+                    $comment_author = User::getMapper('User')->findByEmail($author_email);
+                    if(!is_object($comment_author))
+                    {
+                        $comment_author = new User();
+                        $comment_author->setUsername($author_email);
+                        $comment_author->setFirstName($author_name)->setLastName('');
+                        $comment_author->setDateOfBirth(new DateTime())->setDateJoined(new DateTime());
+                        $comment_author->setPlaceOfOrigin($report->getLocationDistrict())->setPlaceOfResidence($report->getLocationDistrict());
+                        $comment_author->setEmail($author_email);
+                        $comment_author->setUserType($comment_author::USER_TYPE_USER);
+                        $comment_author->setStatus($comment_author::STATUS_INACTIVE);
+                        $comment_author->mapper()->insert($comment_author);
+                    }
+
+                    if(strlen($author_name) and strlen($author_email) and strlen($author_comment))
+                    {
+                        $comment = new Comment();
+                        $comment->setPostId($report->getId());
+                        $comment->setCommentAuthor($comment_author);
+                        $comment->setCommentTime(new DateTime());
+                        $comment->setCommentType(Comment::COMMENT_TYPE_REPORT);
+                        $comment->setContent($author_comment);
+                        $comment->setStatus(Comment::STATUS_APPROVED);
+                        $comment->mapper()->insert($comment);
+
+                        $requestContext->setFlashData('Your comment has been submitted successfully.');
+                        $data['status'] = 1;
+                    }
+                    else
+                    {
+                        $requestContext->setFlashData('Please supply all fields');
+                        $data['status'] = 0;
+                    }
+                }
+
+                $comments = Comment::getMapper('Comment')->findByPost($report->getId());
+
+                $data['reports'] = $report;
+                $data['comments'] = $comments;
+                $requestContext->setView('page-reports-single.php');
+                $requestContext->setResponseData($data);
+                return;
+            }
+        }
+
+        $approved_reports = Report::getMapper('Report')->findByStatus(Report::STATUS_APPROVED);
+        $data['reports'] = $approved_reports;
+        $requestContext->setView('page-reports-list.php');
         $requestContext->setResponseData($data);
-        $requestContext->setView('page-reports.php');
     }
 }
---
+++ Application/Views/page-reports-single.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Phoenix Laboratories NG.
+ * Author: J. C. Nwobodo (jc.nwobodo@gmail.com)
+ * Project: PoliceBlackMarket
+ * Date:    12/3/2015
+ * Time:    7:03 PM
+ **/
+
+$requestContest = \System\Request\RequestContext::instance();
+$data = $requestContest->getResponseData();
+$report = $data['reports'];
+$comments = $data['comments'];
+
+require_once("header.php");
+?>
+<div class="row full-padding-bottom">
+    <div class="col-md-7 col-md-offset-1">
+        <h2 class="page-header"><?= site_info('name'); ?> Report</h2>
+        <article>
+            <h3><?= $report->getTitle(); ?></h3>
+            <p>
+                <span class="glyphicon glyphicon-calendar"></span> <?= $report->getEventTime()->getDateTimeStr(); ?> |
+                <span class="glyphicon glyphicon-map-marker"></span> <?= $report->getLocation(); ?>
+            </p>
+            <div class="text-justify"><?= $report->getDescription(); ?></div>
+        </article>
+        <div id="comments">
+            <form method="post" action="<?php home_url('/reports/?id='.$report->getId().'#comments'); ?>">
+                <fieldset><legend>Comments</legend></fieldset>
+                <?php
+                if(is_object($comments) and $comments->size())
+                {
+                    foreach($comments as $comment)
+                    {
+                        ?>
+                        <div class="full-margin-bottom border-bottom border-color3 border-width-1px">
+                            <div class="row">
+                                <p class="col-xs-6"><span class="glyphicon glyphicon-user"></span> <?= $comment->getCommentAuthor()->getFirstName(); ?></p>
+                                <p class="col-xs-6 text-right"><span class="glyphicon glyphicon-calendar"> </span> <?= $comment->getCommentTime()->getDateTimeStr(); ?></p>
+                            </div>
+                            <p><span class="glyphicon glyphicon-comment"></span> <?= $comment->getContent(); ?></p>
+                        </div>
+                        <?php
+                    }
+                    ?><p class="lead">Your voice counts! Post a reply.</p><?php
+                }
+                else
+                {
+                    ?><p class="lead">Be the first person to comment on this report!</p><?php
+                }
+                ?>
+                <div class="form-group form-group-sm">
+                    <div class="row">
+                        <div class="col-xs-3"><label for="author_name">Name</label></div>
+                        <div class="col-xs-9"><input name="author_name" id="author_name" type="text" class="form-control" placeholder="your name"/></div>
+                    </div>
+                </div>
+                <div class="form-group form-group-sm">
+                    <div class="row">
+                        <div class="col-xs-3"><label for="author_email">Email</label></div>
+                        <div class="col-xs-9"><input name="author_email" id="author_email" type="text" class="form-control" placeholder="your email address"/></div>
+                    </div>
+                </div>
+                <div class="form-group form-group-sm">
+                    <div class="row">
+                        <div class="col-sm-3"><label for="author_comment">Comment</label></div>
+                        <div class="col-sm-9"><textarea name="author_comment" id="author_comment" class="form-control height-10vh" placeholder="what's on your mind?"></textarea></div>
+                    </div>
+                </div>
+                <div class="text-center mid-margin-bottom <?= $data['status'] ? 'text-success bg-success' : 'text-danger bg-danger';?>"><?= $requestContest->getFlashData(); ?></div>
+                <div class="form-group form-group-sm pull-right">
+                    <input name="post_comment" id="post_comment" type="submit" value="Submit Comment" class="btn btn-primary">
+                </div>
+                <span class="clear-both"></span>
+            </form>
+        </div>
+    </div>
+    <div class="col-md-3">
+        sidebar
+    </div>
+</div>
+<?php
+require_once("footer.php");
+?>
